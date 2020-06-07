11085f4f4ffcbff113fe1730460c2a3de358d631
Javier <javier.zen@gmail.com>
2020-06-08 00:25:19 +0200

    Updates

---
+++ precache-manifest.5e1b0417df241f24d73a9da62b283c71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f3436259c2476db26740809aad1e75",
+    "url": "/p5manager/index.html"
+  },
+  {
+    "revision": "7583ecc58225d631cfdc",
+    "url": "/p5manager/static/css/main.e04aa5f6.chunk.css"
+  },
+  {
+    "revision": "53fe9ebf024d12dd66e7",
+    "url": "/p5manager/static/js/2.cf4af3de.chunk.js"
+  },
+  {
+    "revision": "ebc14457535110648808604cc4b96810",
+    "url": "/p5manager/static/js/2.cf4af3de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7583ecc58225d631cfdc",
+    "url": "/p5manager/static/js/main.7548fc22.chunk.js"
+  },
+  {
+    "revision": "89af4d742724c43b3a7c",
+    "url": "/p5manager/static/js/runtime-main.d297b37c.js"
+  }
+]);
